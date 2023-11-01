b2b96e810c184b22702532592bacc92e3d856b7a
Felipe Corredor <pipecorredor06@gmail.com>
2023-10-31 23:39:00 -0500

    catch error

--- pages/api/students/getAll.js
+++ pages/api/students/getAll.js
@@ -1,6 +1,12 @@
 import prisma from "../../../libs/prismadb";
 
 export default async function handle(req, res) {
-  const students = await prisma.student.findMany();
-  res.json(students);
+  try {
+    const students = await prisma.student.findMany();
+
+    res.json(students);
+  } catch (error) {
+    console.error("Error getting students::", error);
+    res.status(500).json({ error });
+  }
 }
