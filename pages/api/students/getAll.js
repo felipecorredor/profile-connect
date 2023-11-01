0a7824fecb6c126bcb6a592cb131d2a65f90422e
Felipe Corredor <pipecorredor06@gmail.com>
2023-10-31 23:51:37 -0500

    prisma client

--- pages/api/students/getAll.js
+++ pages/api/students/getAll.js
@@ -1,5 +1,7 @@
 // import prisma from "../../../libs/prismadb";
 
+import { PrismaClient } from "@prisma/client";
+
 const prisma = new PrismaClient();
 
 export default async function handle(req, res) {
